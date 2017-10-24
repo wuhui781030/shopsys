5227f2fe8a62823ef62e2719479bd922ffa4b6cc
jackywu <wuhui781030@163.com>
2017-10-23 23:02:20 -0700

    修改数据库接口为mysql

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shop_db',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '3306',
     }
 }
 
